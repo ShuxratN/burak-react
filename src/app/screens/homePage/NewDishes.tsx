1d978c9be5109aaa4ebd1a06fef7cdc59a9183a9
ShuxratN <shuxratbeknozimjonov12@gmail.com>
2025-04-23 18:49:43 +0900

    feat: develo newDishes sectional component

--- src/app/screens/homePage/NewDishes.tsx
+++ src/app/screens/homePage/NewDishes.tsx
@@ -1,10 +1,30 @@
 import React from "react";
-import { Container } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
+import AspectRatio from "@mui/joy/AspectRatio";
+import Card from "@mui/joy/Card";
+import CardOverflow from "@mui/joy/CardOverflow";
+import Typography from "@mui/joy/Typography";
+import { CssVarsProvider } from "@mui/joy/styles";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import Divider from " .. / .. /components/divider";
+
+const newDishes = [
+{ productName: "Cutlet", imagePath: "/img/cutlet.webp" },
+{ productName: "Kebab", imagePath: "/img/kebab-fresh.webp" },
+{ productName: "Kebab", imagePath: "/img/kebab.webp" },
+{ productName: "Lavash", imagePath: "/img/lavash.webp" },
 
 export default function NewDishes() {
-    return ( 
-        <div>
-            <Container> NewDishes </Container>
-        </div>
-    );
-}
+ return (
+<div className={"new-products-frame"}>
+<Container>
+<Stack className={"main"}>
+<Box className={"category-title"}>Fresh Menu</Box>
+<Stack className={"cards-frame"}>
+<CssVarsProvider>
+{newDishes.map((ele, index) => {
+return (
+<Card key={index} variant="outlined" className={"card"}>
+<CardOverflow>
+<div className="product-sale">Normal size</div>
+<AspectRatio ratio="1">
