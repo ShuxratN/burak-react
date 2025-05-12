130995c93ecc5a780ea49a551f622700823698c4
ShuxratN <shuxratbeknozimjonov12@gmail.com>
2025-05-12 18:48:38 +0900

    feat: Practice useState hooks

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -1,10 +1,16 @@
 import { Box, Button, Container, Stack } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import Basket from "./Basket";
+import { useState } from "react";
 
 export default function HomeNavbar () {
- 
     const authMember = null;
+    const [count, setCount] = useState(0);
+
+    const buttonHandler = () => {
+        setCount(count + 1 );
+    };
+
     return ( 
     <div className="home-navbar">
         <Container className="navbar-container">
@@ -62,11 +68,14 @@ export default function HomeNavbar () {
                         One of the  most Delicious Coisine
                     </Box>
                     <Box className={"wel-txt"}>The Choise, not just a choise</Box>
-                    <Box className={"service-txt"}>24 hours service</Box>
+                    <Box className={"service-txt"}> { count } hours service</Box>
                     <Box className={"signup"}>
                         {!authMember ? (
-                        <Button variant={"contained"}
-                        className={"signup-button"}> 
+                        <Button 
+                        variant={"contained"}
+                        className={"signup-button"}
+                        onClick={buttonHandler}
+                        > 
                         Sign UP </Button> ) : null }
                     </Box>
                 </Stack>
